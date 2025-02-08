412269286e15bec326f6a45c2fb0eea7c5d76ccc
S Chand Basha <136245799+chandsk03@users.noreply.github.com>
2025-02-08 23:03:40 +0530

    Update config.py

--- app/config.py
+++ app/config.py
@@ -6,4 +6,4 @@ load_dotenv()
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "supersecretkey")
     MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://chand37880:4vMXUSi2MyoVd3FY@cluster0.gegvaeq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-    TELEGRAM_BOT_TOKEN = os.getenv("8103505258:AAFjPTARUAHxoX5KAJPg71RWT7_AULfKd0k")
+    TELEGRAM_BOT_TOKEN = os.getenv("3")
